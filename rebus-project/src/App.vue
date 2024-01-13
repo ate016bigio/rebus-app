af4af5a1481abfb54f68466e513c424611794377
ate016bigio <alessandrob.bianchi@gmail.com>
2024-01-13 10:40:22 +0100

    First steps involving props, components, ...

--- rebus-project/src/App.vue
+++ rebus-project/src/App.vue
@@ -3,16 +3,30 @@
     <h1>
       {{ title }}
     </h1>
+    <div v-if="showModal">
+      <Modal :header="modal_header" :text="modal_text" :theme="modal_theme" @close="toggleModal"/>
+    </div>
   </div>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 // Root component
 export default {
   name: 'App',
+  components: {Modal},
   data() {
     return {
-      title: '✨ Welcome to the Rebus Application ✨'
+      title: '✨ Welcome to the Rebus Application ✨',
+      modal_header: "La zia",
+      modal_text: "la sa",
+      modal_theme: "sale",
+      showModal: true
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
     }
   }
 }
